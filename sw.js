b7f965e8f6bbb1f687d0c338affa7c6ece458897
thore-fuglestad <72924773+thore-fuglestad@users.noreply.github.com>
2026-03-06 07:43:07 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+var CACHE = 'timer-v1';
+var FILES = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
+});
